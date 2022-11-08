d0ba077be10df9ba70289cba71e3564ec2a821e1
phrdc <j.carillo@prestigehomesrealty.com.ph>
2022-11-08 18:29:45 +0800

    Create TestController.cs

---
+++ HCMServer/HCMServer/Apis/V1/Controllers/Test/TestController.cs
@@ -0,0 +1,61 @@
+﻿namespace HCMServer.Apis.V1.Controllers.Test;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class TestsController : ControllerBase
+{
+    private readonly ISender _mediator;
+    private readonly IMapper _mapper;
+
+    public TestsController(ISender mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        GetPaymentParticularsQuery q = new GetPaymentParticularsQuery();
+
+        var result = await _mediator.Send(q).ConfigureAwait(false);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(PaymentParticularModel o)
+    {
+        var command = _mapper.Map<PaymentParticularPostCommand>(o);
+
+        var result = await _mediator.Send(command).ConfigureAwait(false);
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, PaymentParticularModel o)
+    {
+        if (id != o.Id)
+            return BadRequest();
+
+        var command = _mapper.Map<PaymentParticularPutCommand>(o);
+
+        var result = await _mediator.Send(command).ConfigureAwait(false);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{ids}")]
+    public async Task<IActionResult> DeleteAsync(int[] ids)
+    {
+        var command = new PaymentParticularDeleteCommand
+        {
+            Ids = ids
+        };
+
+        var result = await _mediator.Send(command).ConfigureAwait(false);
+
+        return Ok(result);
+    }
+}
